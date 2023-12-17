ef70af466a9658c16911f73379792bff6dadd24f
dchis19 <116224247+dchis19@users.noreply.github.com>
2023-12-16 23:17:46 -0500

    Update binaryTree.py

--- binaryTree.py
+++ binaryTree.py
@@ -1,8 +1,18 @@
+"""
+Node class is used to store instances of each binaryTree
+"""
+
 class Node:
 	def __init__(self, key):
 		self.val = key
 		self.right = None
 		self.left = None
+
+"""
+Insert(root, key) adds the key value into the binary tree. If root is None (ie top of tree), then we make the binary tree. If the
+value is less than the key, we add the value to the right side of the node. If the root is larger than the key, we add the value to 
+the left side of the node.
+"""
 
 def insert(root, key):
 	if root is None:
@@ -16,11 +26,20 @@ def insert(root, key):
 			root.right = insert(root.right, key)
 	return root
 
+"""
+inorder(root) iterates through the binary tree and prints the values in the order from smallest to largest
+"""
+
 def inorder(root):
 	if root:
 		inorder(root.left)
 		print(root.val, end = " ")
 		inorder(root.right)
+
+"""
+search(root, key) performs a search of the tree based on a key value. If the key value is found in the tree, the root is returned.
+Otherwise None is returned as the entire tree is iterated through. 
+"""
 
 def search(root, key):
 	if root is None or root.val == key:
